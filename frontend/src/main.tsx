746f8e9d3de8c308af1fd8cac36ac77728f79bc0
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-19 11:01:05 +0500

    search bar component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "react-query";
 import { AppContextProvider } from "./contexts/AppContexts.tsx";
+import { SearchContextProvider } from "./contexts/SearchContext.tsx";
 import App from './App.tsx'
 import './index.css'
 
@@ -20,7 +21,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
          <AppContextProvider>
+          <SearchContextProvider>
           <App />
+        </SearchContextProvider>
       </AppContextProvider>
     </QueryClientProvider>
   </React.StrictMode>,
