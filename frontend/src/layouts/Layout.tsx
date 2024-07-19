59957f055ff697f1c72e626327a227b74703a0bc
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-07-19 11:12:20 +0500

    search page

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -2,6 +2,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
 // import Hero from "../components/Hero";
 // import SearchBar from "../components/SearchBar";
 
@@ -16,6 +17,9 @@ const Layout = ({ children }: Props) => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero/>
+       <div className="container mx-auto">
+        <SearchBar/>
+      </div>
       <div className="container mx-auto py-10 flex-1">{children}</div>
       <Footer/>
     </div>
